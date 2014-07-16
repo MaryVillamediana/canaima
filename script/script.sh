e9470d608a008a7f8737928a5554816f168d9219
Mary <mary25villamediana@gmail.com>
2014-07-16 14:39:35 -0430

    subiendo crontab

--- script/script.sh
+++ script/script.sh
@@ -2,7 +2,6 @@
 fecha=$(date +"%m-%d-%Y-%T")
 echo *******RESPALDO******
 cd /home/academia
-mkdir respaldo1
 cp -r /home/academia/respaldo/ /home/academia/respaldo1/respaldo$fecha
 ******fin******
 
